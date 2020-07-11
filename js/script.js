358fbe1a3f0504f624c24750d8bfe91d5edc344c
Alex <daos147@gmail.com>
2020-07-11 14:15:57 +0300

    create comments form on Javascript

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Comments
+let comments =[];
+loadComments();
+
+document.getElementById('comment-add').onclick = function() {
+  event.preventDefault();
+  let commentName = document.getElementById('comment-name');
+  let commentBody = document.getElementById('comment-body');
+
+  let comment = {
+    name : commentName.value,
+    body : commentBody.value,
+    time : Math.floor(Date.now()/1000)
+  }
+  commentName = '';
+  commentBody = '';
+  comments.push(comment);
+  saveComments();
+  showComments();
+}
+function saveComments() {
+  localStorage.setItem('comments', JSON.stringify(comments));
+}
+function loadComments() {
+  if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
+  showComments();
+}
+function showComments() {
+  let commentField = document.getElementById('comment-field');
+  let out = '';
+  comments.forEach(function(item) {
+    out += `<p class="text-right small"><em>${timeConverter(item.time)}</em></p>`;
+    out += `<p class="alert alert-primary">${item.name}</p>`;
+    out += `<p class="alert alert-success">${item.body}</p>`;
+  });
+  commentField.innerHTML = out;
+}
+function timeConverter(UNIX_timestamp) {
+  let a = new Date(UNIX_timestamp * 1000);
+  let months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+  let year = a.getFullYear();
+  let month = months[a.getMonth()];
+  let date = a.getDate();
+  let hour = a.getHours();
+  let min = a.getMinutes();
+  let sec = a.getSeconds();
+  let time = `${date} ${month} ${year}г. ${hour}:${min}:${sec}`;
+  return time;
+}
